41e9a881ac6187336c55b137239f68bc049f7cf9
Richard Canoa <Richard@MacBook-Richard.local>
2015-05-20 16:13:18 -0400

    Initial Commit

---
+++ AvisosApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AvisosApp
+//
+//  Created by Richard Canoa on 20/5/15.
+//  Copyright (c) 2015 Richard Canoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AvisosAppTests/AvisosAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  AvisosAppTests.swift
+//  AvisosAppTests
+//
+//  Created by Richard Canoa on 20/5/15.
+//  Copyright (c) 2015 Richard Canoa. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class AvisosAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
